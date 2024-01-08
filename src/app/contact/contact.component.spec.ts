4519dcd0e6c739dd953fb807db19dff2a7cfbb70
Farhanaaryan <itzfarhan24@gmail.com>
2024-01-08 11:44:35 +0530

    Unit test and css

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContactComponent } from './contact.component';
+import { FormsModule } from '@angular/forms';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,9 +8,10 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ContactComponent],
+      imports: [FormsModule],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +20,54 @@ describe('ContactComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize with empty fields', () => {
+    expect(component.firstName).toEqual('');
+    expect(component.lastName).toEqual('');
+    expect(component.country).toEqual('india');
+    expect(component.message).toEqual('');
+    expect(component.isSubmitted).toBeFalse();
+  });
+
+  it('should reset fields and set isSubmitted to true on form submission', () => {
+    spyOn(window, 'alert');
+    component.firstName = 'John';
+    component.lastName = 'Doe';
+    component.country = 'usa';
+    component.message = 'Test message';
+    component.OnSubmit();
+
+    expect(component.firstName).toEqual('');
+    expect(component.lastName).toEqual('');
+    expect(component.country).toEqual('india');
+    expect(component.message).toEqual('');
+    expect(component.isSubmitted).toBeTrue();
+    expect(window.alert).toHaveBeenCalledWith('Thank you for your response ');
+  });
+
+  it('should return true on canExit if there are no unsaved changes', () => {
+    const result = component.canExit();
+    expect(result).toBeTrue();
+  });
+
+  it('should return confirmation prompt on canExit if there are unsaved changes', () => {
+    component.firstName = 'John';
+    const spy = spyOn(window, 'confirm').and.returnValue(true);
+    const result = component.canExit();
+
+    expect(result).toBeTrue();
+    expect(spy).toHaveBeenCalledWith('You have unsaved changes. Do you want to navigate away?');
+  });
+
+  it('should return true on canExit if there are unsaved changes but user cancels navigation', () => {
+    component.firstName = 'John';
+    const spy = spyOn(window, 'confirm').and.returnValue(false);
+    const result = component.canExit();
+
+    expect(result).toBeFalse();
+    expect(spy).toHaveBeenCalledWith('You have unsaved changes. Do you want to navigate away?');
   });
 });
